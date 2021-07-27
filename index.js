27f5276be20f0589edf6d07e55564dffc1b49fd8
Edward Thomson <ethomson@edwardthomson.com>
2021-07-27 10:13:27 -0400

    tls-test for a TLS 1.2 endpoint
    This version of the tls-test package will not be published to the public
    npm registry, it will only be published to an HTTPS endpoint or registry
    that enforces TLS 1.2.

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 
 'use strict'
 
-console.log('Hello!  This package is meant to be downloaded over an HTTPS channel that')
-console.log('enforces TLS 1.2, to test whether you\'re ready for upgrades to the public')
-console.log('npm registry.  To do this, run:')
+console.log('Hello! The tls-test package was successfully downloaded and installed.')
 console.log('')
-console.log('npm install -g https://tls-test.npmjs.com/tls-test-1.0.0.tgz')
-console.log('')
-console.log('If that command succeeds then you\'re ready for TLS 1.2 for npmjs.com.')
-console.log('For more information, see https://github.com/npm/tls-test#readme')
+console.log('Congratulations! Your package manager appears to support TLS 1.2.')
 console.log('')
